08d9708b59696de406b320a50c1aa2a8d912e153
lisuseq <pawelisus123@gmail.com>
2026-05-19 21:54:36 +0200

    added bridge file with basic function declaration

---
+++ bridge.h
@@ -0,0 +1,3 @@
+void initBridge();
+void depart();
+void exitBridge();
---
+++ brigde.c
@@ -0,0 +1,14 @@
+#include "bridge.h"
+#include <pthread.h>
+
+void initBridge(){
+    // code to initialize the bridge
+}
+
+void depart(){
+    // code to start departure from one side of the bridge
+}
+
+void exitBridge(){
+    // code to exit the bridge
+}
