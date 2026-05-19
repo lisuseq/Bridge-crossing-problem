38e8f66f225a0ec6347c98861bd37123bf3ea49d
lisuseq <pawelisus123@gmail.com>
2026-05-19 22:33:48 +0200

    added some of the key variables

--- brigde.c
+++ brigde.c
@@ -1,8 +1,17 @@
 #include "bridge.h"
 #include <pthread.h>
+#include <stdbool.h>
+
+#define LEFT 0
+#define RIGHT 1
+static bool vehicle_present = false;
+static int Waiting[2]; // number of vehicles waiting on each side
 
 void initBridge(){
     // code to initialize the bridge
+    vehicle_present = false;
+    Waiting[LEFT] = 0;
+    Waiting[RIGHT] = 0;
 }
 
 void depart(){
